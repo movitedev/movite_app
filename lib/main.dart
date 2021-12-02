525186034a3ed06002e20181473b0913818c14be
emiliantolo <movitedev@gmail.com>
2021-12-02 14:31:42 +0100

    Changed name

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Flutter login demo',
+        title: 'Movite',
         debugShowCheckedModeBanner: false,
         theme: new ThemeData(
           primarySwatch: Colors.blue
